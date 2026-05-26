6c5fe176476e6a8b38608537cf7384a6dc6db1b6
pyogeswarsai <pyogeswarsai1249@gmail.com>
2026-05-26 06:26:26 +0530

    finl changed

--- src/main/java/Pages/CheckoutPage.java
+++ src/main/java/Pages/CheckoutPage.java
@@ -18,7 +18,7 @@ public class CheckoutPage {
 
     public CheckoutPage(WebDriver driver) {
         this.driver = driver;
-        this.wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(25));
         PageFactory.initElements(driver, this);
     }
 
